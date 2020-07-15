dedd691c3b99c9670dcf71db76272521c1985366
Sanji Jin <bowindow32@gmail.com>
2020-07-15 21:24:36 +0200

    Añadido proyecto de test con primer test

---
+++ SimpleRPG.Game.Tests/TestClassTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Xunit;
+
+namespace SimpleRPG.Game.Tests
+{
+    public class TestClassTests
+    {
+        [Fact]
+        public void DoSomething_Test() 
+        {
+            //Arrange
+            var cut = new TestClass();
+
+            // Act
+            var result = cut.DoSomething("TestAbc");
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal("TestAbc", cut.Name);
+        }
+    }
+}
---
+++ SimpleRPG.Game/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRPG.Game
+{
+    public class TestClass
+    {
+        public string Name { get; private set; } = string.Empty;
+
+        public bool DoSomething(string value) 
+        {
+            this.Name = value;
+            return true;
+        }
+    }
+}
