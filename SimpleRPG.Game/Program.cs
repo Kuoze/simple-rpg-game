57c9faa81230a150da2e34e552fa86d8ca86e15e
Sanji Jin <bowindow32@gmail.com>
2020-07-15 21:04:28 +0200

    Configurar Proyecto para usar Blazorise

--- SimpleRPG.Game/Program.cs
+++ SimpleRPG.Game/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
 
 namespace SimpleRPG.Game
 {
@@ -15,11 +17,20 @@ namespace SimpleRPG.Game
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            // Add Blazorise services
+            builder.Services.AddBlazorise(options =>
+            {
+                options.ChangeTextOnKeyPress = false;
+            }).AddBootstrapProviders();
+
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var host = builder.Build();
 
-            await builder.Build().RunAsync();
+            host.Services.UseBootstrapProviders();
+
+            await host.RunAsync().ConfigureAwait(false);
         }
     }
 }
