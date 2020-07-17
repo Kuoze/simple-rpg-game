76919f4400b5082b8f60c1fe74cdac6fd7e39296
Sanji Jin <bowindow32@gmail.com>
2020-07-17 20:07:27 +0200

    Added Initial GameEngine with initial test

---
+++ SimpleRPG.Game.Engine.Tests/PlayerTests.cs
@@ -0,0 +1,36 @@
+﻿using SimpleRPG.Game.Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SimpleRPG.Game.Engine.Tests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void CreateSimplePlayer() 
+        {
+            // Arrange
+
+            // Act
+            var p = new Player()
+            {
+                Name = "Test",
+                Level = 1,
+                HitPoints = 10
+            };
+
+            // Assert
+            Assert.NotNull(p);
+            Assert.Equal("Test", p.Name);
+            Assert.Equal(string.Empty, p.CharacterClass);
+            Assert.Equal(1, p.Level);
+            Assert.Equal(10, p.HitPoints);
+            Assert.Equal(0, p.ExperiencePoints);
+            Assert.Equal(0, p.Gold);
+        }
+
+
+    }
+}
---
+++ SimpleRPG.Game.Engine/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleRPG.Game.Engine.Models
+{
+    public class Player
+    {
+        public string Name { get; set; } = string.Empty;
+        public string CharacterClass { get; set; } = string.Empty;
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
