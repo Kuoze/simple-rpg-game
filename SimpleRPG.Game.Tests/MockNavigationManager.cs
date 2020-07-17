1baad9d4515b6458e09b20a99665d361624a4669
Sanji Jin <bowindow32@gmail.com>
2020-07-17 19:33:46 +0200

    Test Classes and samples

---
+++ SimpleRPG.Game.Tests/MockNavigationManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleRPG.Game.Tests
+{
+    public class MockNavigationManager : NavigationManager
+    {
+        public MockNavigationManager() 
+        {
+            this.Initialize("https://test.com/", "https://test.com/testlink/");
+        }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            this.Uri = uri;
+        }
+    }
+}
---
+++ SimpleRPG.Game.Tests/NavMenuTests.cs
@@ -0,0 +1,47 @@
+﻿using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleRPG.Game.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SimpleRPG.Game.Tests
+{
+    public class NavMenuTests
+    {
+        [Fact]
+        public void SimpleRender() 
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            ctx.Services.AddSingleton<NavigationManager>(new MockNavigationManager());
+
+            // Act
+            var cut = ctx.RenderComponent<NavMenu>();
+
+            // Assert
+            var expected = @"<span class=""oi oi-home"" aria-hidden=""true""></span> Home";
+            Assert.Contains(expected, cut.Markup);
+            expected = @"<div class=""collapse"" blazor:onclick=""2"">";
+            Assert.Contains(expected, cut.Markup);
+        }
+
+        [Fact]
+        public void ToggleNavMenu() 
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            ctx.Services.AddSingleton<NavigationManager>(new MockNavigationManager());
+
+            // Act
+            var cut = ctx.RenderComponent<NavMenu>();
+            cut.Find(".navbar-toggler").Click();
+
+            // Assert
+            var expected = @"<div blazor:onclick=""2"">";
+            Assert.Contains(expected, cut.Markup);
+        }
+    }
+}
